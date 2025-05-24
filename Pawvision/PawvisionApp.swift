d2bc123ce84d747944aaf2365e8d88e0511096e2
tuachout <vikrant.thakur@gmail.com>
2025-05-24 12:12:45 -0700

    Initial Commit

---
+++ Pawvision/PawvisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawvisionApp.swift
+//  Pawvision
+//
+//  Created by Vikrant Singh on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct PawvisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawvisionTests/PawvisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  PawvisionTests.swift
+//  PawvisionTests
+//
+//  Created by Vikrant Singh on 5/24/25.
+//
+
+import Testing
+@testable import Pawvision
+
+struct PawvisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
